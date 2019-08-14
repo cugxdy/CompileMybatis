720d051ac090d2600620576dcadd1002ed581dcd
cugxdy <cugxdy@163.com>
2019-08-14 19:06:27 +0800

    它是对创建ResultMap对象所需进行总览

--- src/main/java/org/apache/ibatis/builder/ResultMapResolver.java
+++ src/main/java/org/apache/ibatis/builder/ResultMapResolver.java
@@ -24,6 +24,7 @@ import org.apache.ibatis.mapping.ResultMapping;
 /**
  * @author Eduardo Macarron
  */
+// 
 public class ResultMapResolver {
   private final MapperBuilderAssistant assistant;
   private final String id;
