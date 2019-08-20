d4b5f3a7993c7742c6416097d666ec9281b0cbca
cugxdy <cugxdy@163.com>
2019-08-20 17:16:02 +0800

    批处理异常类

--- src/main/java/org/apache/ibatis/executor/BatchExecutorException.java
+++ src/main/java/org/apache/ibatis/executor/BatchExecutorException.java
@@ -26,6 +26,7 @@ import java.util.List;
  * 
  * @author Jeff Butler
  */
+// 批处理异常类
 public class BatchExecutorException extends ExecutorException {
 
   private static final long serialVersionUID = 154049229650533990L;
